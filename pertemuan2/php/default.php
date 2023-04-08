7e68f9d0ba190bd27803313aed26a144cb5c36c3
srik <85044480+sriiassa@users.noreply.github.com>
2023-04-08 19:46:39 +0800

    dasar php, percabangan, dan perulangan
    - IF ELSE digunakan untuk menentukan kondisi yang diinginkan
    - SWITCH digunakan jika suatu kondisi benar maka blok bernilai benar akan dijalankan

---
+++ pertemuan2/php/default.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Belajar Script PHP <br>";
+    echo "================================= <br>";
+    $nama   = "Srik";
+    $rank   = 3;
+    echo "Mahasiswa atas nama $nama, meraih peringkat ke-$rank <br>";
+    echo 'Mahasiswa atas nama '.$nama.', meraih peringkat ke-'.$rank;
