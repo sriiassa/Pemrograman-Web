6a2dffc0945f8d7fe8dfe70136bc1032476f47d7
srik <85044480+sriiassa@users.noreply.github.com>
2023-04-08 19:23:01 +0800

    sintaks dasar HTML, CSS, dan JS

---
+++ pertemuan1/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Pertemuan 1 | HTML</title>
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+    <div class="main">
+        <img class="gambar" src="img/profile.jpg" alt="Profile" width="250px">
+        <h1>Selamat Datang Di Pemrograman Web</h1>
+        <!-- <script>
+            document.write("<h5>Mohon untuk menggunakan website ini dengan baik ya<sup>2</sup></h5>")
+        </script> -->
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint numquam ut sapiente tempora dolorem rem dolores odio ex labore modi officia inventore, in a laborum eos quia nulla alias distinctio nam officiis neque reiciendis quo voluptate? Amet quasi facere exercitationem soluta atque ab quam vero eveniet molestias molestiae, harum unde ipsum quidem quaerat doloribus error reprehenderit ea aliquid dolores repellendus in mollitia quia numquam? Quasi fuga architecto distinctio totam, voluptate molestias facilis voluptates laborum deleniti quo! Placeat tempora nisi velit neque sint libero asperiores quis repellendus, cumque quo enim ipsa quos magnam illo beatae voluptates voluptas adipisci magni facere natus!</p>
+        <button class="login">Sign In</button>
+    </div>
+    
+
+    <!-- Javascript -->
+    <script src="main.js"></script>
+    <br>
+
+    <!-- PHP -->
+    <?php
+        // echo "<h3>Kelas U</h3>"
+    ?>
+</body>
+</html>
---
+++ pertemuan1/main.js
@@ -0,0 +1,1 @@
+// alert("Selamat Datang di Kelas U");
